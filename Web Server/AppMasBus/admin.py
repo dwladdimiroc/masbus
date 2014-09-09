1bd08c4903b89245143480dac670290072d2a0c8
dwladdimiroc <daniel.wladdimiro@gmail.com>
2014-09-09 18:26:48 -0300

    Remove folder

--- Web Server/AppMasBus/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from AppMasBus.models import Viaje, Empresa, Ciudad, Convenio, Infraccion
- 
-admin.site.register(Viaje)
-admin.site.register(Ciudad)
-admin.site.register(Empresa)
-admin.site.register(Convenio)
-admin.site.register(Infraccion)
