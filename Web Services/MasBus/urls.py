7074b39785a6c943b25da3f3b3aabb10d22dd516
dwladdimiroc <daniel.wladdimiro@gmail.com>
2014-09-09 18:25:32 -0300

    Upload name

---
+++ Web Services/MasBus/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'MasBus.views.home', name='home'),
+    # url(r'^MasBus/', include('MasBus.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^AppMasBus/', include('AppMasBus.urls', namespace = "AppMasBus")),
+    
+)
